a5d0b1b97fa9250af0c41c8cbce00c618dab654e
That is me <trab.die@gmail.com>
2021-11-27 09:07:45 -0500

    hashmap phonebook

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -16,39 +16,61 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+
+        this.phonebook = new phonebook;
     }
 
     public PhoneBook() {
+
+        this.phonebook=
         this(null);
     }
 
+    // adds an entry to the composite associate data type
     public void add(String name, String phoneNumber) {
+        list.add (phoneNumber);
+        map.put(name,Array.asList (phonenumber));
+
     }
 
+    // adds many phone numbers to a single name entry
     public void addAll(String name, String... phoneNumbers) {
+
     }
 
+    // removes an entry to the composite associate data type
     public void remove(String name) {
+
     }
 
+    // removes an entry to the composite associate data type
     public Boolean hasEntry(String name) {
+
         return null;
     }
 
+
+    // returns a phone number for the respective input name
     public List<String> lookup(String name) {
+
         return null;
     }
 
+
+    // returns a name for the respective input phoneNumber
     public String reverseLookup(String phoneNumber)  {
+
         return null;
     }
 
+    // returns a list of all names in this PhoneBook
     public List<String> getAllContactNames() {
+
         return null;
     }
 
     public Map<String, List<String>> getMap() {
+
         return null;
     }
 }
